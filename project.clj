8c621652e34bd82f960b7b81cb31d5a54a6f0b44
Tim Clemons <tclemons@gmail.com>
2011-06-14 21:00:28 -0700

    Added README and project.clj.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject cluje "0.1.0"
+  :description "A library to make the JRE ScriptEngine framework more convenient for Clojure developers."
+  :url "http://github/timclemons/cluje"
+  :dependencies
+    [[org.clojure/clojure "1.2.0"]
+     [org.jruby/jruby "1.6.2"]])
+
